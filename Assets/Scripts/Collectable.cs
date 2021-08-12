ae661458900ac586a0a80addbcad66b7f8ff33f3
sunny-Dev7 <dev7.sunny@gmail.com>
2021-08-13 00:28:50 +0900

    First commit

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : Collidable
+{
+    // Logic
+    protected bool collected;
+
+    protected override void Oncollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+            Oncollect();
+    }
+
+    protected virtual void Oncollect()
+    {
+        collected = true;
+    }
+}
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+
+    public static GameManger instance;
+    private void Awake()
+    {
+        if(GameManger.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Ressources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public player player;
+    // public weapon weapon...
+    public FlotingTextManager floatingTextManager;
+
+
+    // Logic
+    public int pesos;
+    public int experience;
+
+    // Floating text
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+    // Save state 
+    /*
+     * INT perferedSkin
+     * INT pesos
+     * INT experience
+     * INT weponelevel
+     */
+    public void SaveState()
+    {
+        Debug.Log("SaveState");
+    }
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        Debug.Log("LoadState");
+    }
+
+}
