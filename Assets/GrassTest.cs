286eeb7faf6f3ae3bf40eae8f975e8238bc4fc3c
sunny-Dev7 <dev7.sunny@gmail.com>
2021-09-05 19:40:44 +0900

    Update GrassTest.cs

--- Assets/GrassTest.cs
+++ Assets/GrassTest.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class GrassTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //잔디심기테스트입니당.
 }
