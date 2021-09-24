c65aa13eae89def0cb7fa8252a84be69f8419a37
sunny-Dev7 <dev7.sunny@gmail.com>
2021-09-24 22:05:26 +0900

    Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector3 leftRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        leftRight = new Vector3(x, y, 0);
+
+        if (leftRight.x > 0)
+            transform.localScale = Vector3.one;
+        else if (leftRight.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        transform.Translate(leftRight * Time.deltaTime);
     }
 }
