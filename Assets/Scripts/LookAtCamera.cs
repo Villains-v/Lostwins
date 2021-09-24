7699bbd77b8572c1620b3059357dd326b982452a
sunny-Dev7 <dev7.sunny@gmail.com>
2021-09-24 21:47:47 +0900

    Modify camera script

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject player;
     public Transform target;
     public float smoothSpeed = 0.125f;
 
@@ -12,7 +12,9 @@ public class LookAtCamera : MonoBehaviour
     {
         // transform.LookAt(target, Vector3.zero); -> 타겟을 따라서 XYZ가 같이 움직임
         // transform.position = target.transform.position; -> 여기 position Z에  -1을 넣고싶은데 하는 방법을 모르겠음
-        transform.position = Player.transform.position + new Vector3(0,0,-1);
+        //transform.position = player.transform.position + new Vector3(0,0,-1);
+
+        transform.position = new Vector3(target.position.x, target.position.y, -1);
 
     }
 }   
